024cd051e025dc1fa7c068a2bd50d84114dac68c
AmberDube <amber.dube@codesquaretech.com>
2024-02-27 15:23:26 +0530

    add video functionality

--- backend/routes/user/user.js
+++ backend/routes/user/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userrouter = express.Router();
 const usercontroller = require('../../controller/user/user');
-
+const auth =require('../../middleware/auth')
 userrouter.post('/user/signup', usercontroller.signup);
 userrouter.post('/user/login',usercontroller.login)
 userrouter.post('/generateOtp',usercontroller.verify)
+userrouter.get('/user/detail',auth,usercontroller.getAllUser)
 module.exports = userrouter;
