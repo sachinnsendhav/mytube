0da9d775729f0a7c38eaba05cd66367f063c7c4e
Divyansh-Csqt <109141777+Divyansh-Csqt@users.noreply.github.com>
2024-02-29 14:00:41 +0530

    Delete frontend directory

--- frontend/src/services/index.js
+++
@@ -1,7 +0,0 @@
-export * as Users from "./usersService"
-export * as Auth from './authService';
-export * as YoutubeApi from './youtubeApi';
-export * as Playlist from './playlistService'
-export * as Video from './videoService';
-export * as Subscription from './subscriptionService';
-export * as Channel  from './channelService';
